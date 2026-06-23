0f2f46e9d50609a68ca017b81d280a580f589046
Suman <pasisuman2511@gmail.com>
2026-06-23 17:19:05 +0530

    JWT Token

---
+++ auth-service/src/main/java/com/kodewala/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.kodewala.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.kodewala.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+	private final UserRepository repository;
+
+	public CustomUserDetailsService(UserRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		com.kodewala.entity.User user = repository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+
+		return org.springframework.security.core.userdetails.User.withUsername(user.getUsername())
+				.password(user.getPassword()).roles(user.getRole()).build();
+	}
+}
